b0732a0b6adf78ed138d265d103def56988b2b18
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-01-26 11:48:16 -0300

    Desenvolvimento do primeiro projeto em C# paare voluir profissionalmente, dando o meu melhor a cada dia!

--- Aula01/Program.cs
+++ Aula01/Program.cs
@@ -7,6 +7,27 @@ namespace Aula01
         static void Main(string[] args)
         {
             Console.WriteLine("Ola Mundo!");
+
+            Console.WriteLine("----------------------------------------------");
+
+            string nome = "Gustavo";
+            Console.WriteLine("Olá, " + nome + "!");
+
+            Console.WriteLine("-----------------------------------------------");
+
+            Console.WriteLine("Digite um número:");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("O número " + numero + " é par.");
+            }
+            else
+            {
+                Console.WriteLine("O número " + numero + " é ímpar.");
+            }
+
+
         }
     }
 }
