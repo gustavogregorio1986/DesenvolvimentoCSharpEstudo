4faa78e8d30bae25a73544bf8dcef0edf3601029
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-01-26 14:38:35 -0300

    Desenvolver ainda mais a logica de programação como profissional!

---
+++ Aula03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Aula03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte n1 = 10; // 0- 255
+            //int num=-10;
+            //char letra = 'c';
+            //float valor = 3.5f;
+            //string nome = "Bruno";
+
+            //var aux = nome;
+
+            //Console.WriteLine("Valior da variavel: " + aux + "...");
+
+            int num1, num2, res;
+
+            num1 = 10;
+            num2 = 20;
+
+            //res = num1 * num2;
+
+
+            //Console.WriteLine("A soma de " + num1 + "com " + num2 + " é igual a: " + res);
+
+            Console.WriteLine("A soma de " + num1 + "com " + num2 + " é igual a: " + num1*num2);
+        }
+    }
+}
