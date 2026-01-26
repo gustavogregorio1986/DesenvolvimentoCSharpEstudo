6a7c0960de928319291972c0d94aae8be32b2563
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-01-26 17:22:44 -0300

    Desenvolver a logica no projeto

---
+++ Aula05/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Aula05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int res = (10+5)*2;
+            //int res = 10 + 5 * 2;
+
+            //bool res = 10<5;
+
+            //bool res = 10>5;
+
+            //bool res = 10!=5;
+
+
+            //Console.WriteLine(res);
+
+
+            //num++; //num += 1;  num = num + 1;
+            
+
+            //Console.WriteLine(num);
+
+            bool res = !(10 > 5); // false
+
+            Console.WriteLine(res);
+        }
+    }
+}
