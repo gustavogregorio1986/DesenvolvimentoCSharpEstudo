cf7ca039c817025c29db83b2b3bc668d2ec15173
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-01-26 13:24:01 -0300

    Desenvolvimento da logica de programalção no projeto

---
+++ Aula02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            args = new string[] { "CPB Cursos" };
+            if(args.GetLength(0) > 0)
+            {
+                Console.Write(args.GetValue(0));
+            }
+        }
+    }
+}
