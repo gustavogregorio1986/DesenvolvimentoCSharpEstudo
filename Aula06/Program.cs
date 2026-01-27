fa70d5d82662870c0473df4e4a3b0fdb92d16f31
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-01-27 20:36:24 -0300

    Logica de desenvolvimento de projetos

---
+++ Aula06/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aula06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3;
+
+            n1 = 10; n2 = 20; n3 = 30;
+
+            Console.WriteLine(n1 + "," + n2 + "," + n3);
+
+            Console.WriteLine("n1=\t{0}\n n2=\t{1}\n n3=\t{2}\n", n1, n2,n3);
+
+            double valorCompra = 5.50;
+            double valorVenda;
+            double lucro=0.1;
+            string produto="Pastel";
+
+            valorVenda = valorCompra + (valorCompra * lucro);
+
+            Console.WriteLine("Produto................{0,15}", produto);
+            Console.WriteLine("Val.Compra.............{0,15:c}", valorCompra);
+            Console.WriteLine("Lucro..................{0,15:p}", lucro);
+            Console.WriteLine("Val.Venda..............{0,15:c}", valorVenda);
+        }
+    }
+}
