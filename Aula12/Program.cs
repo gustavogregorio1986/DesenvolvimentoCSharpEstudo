b5baea78c6537290011e3911bdf10ea7f7ae246c
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-01-28 17:26:37 -0300

    Desenvolver é uma arte maravilhosa!

---
+++ Aula12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Aula12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SE (E_L{}
+            //> m >= >= == !-
+
+            int n1, n2, n3, n4, res;
+            res=n1=2=n3=n4=0;
+
+            Console.WriteLine("Digite a nota 1: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a nota 2: ");
+            
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a nota 3: ");
+            
+            n3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a nota 4: ");
+            
+            n4 = int.Parse(Console.ReadLine());
+            string resultado = "Reprovado";
+
+            res=n1+n2+n3+n4;
+
+            if (res > 60){
+                resultado = "Aprovado";
+            }
+
+            Console.WriteLine("Nota: {0} - Resultado:{1}", res, resultado);
+        }
+    }
+}
