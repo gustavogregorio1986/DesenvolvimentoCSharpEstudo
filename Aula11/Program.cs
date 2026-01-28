06e977d7d599e9e3b431cc8e59ddbc5e9d3f265d
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-01-28 15:15:39 -0300

    Desenvolvimento de primitivos paare voluir profissionalmente!

---
+++ Aula11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aula11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int n1 = 10;
+            //float n2 = n1;
+            //Console.WriteLine(n2);
+
+            //float n1 = 10.7f;
+
+            //int n2 = (int)n1;
+
+            //Console.WriteLine(n2);
+
+            //int vInt = 10;
+            //long vLong = vInt;
+
+            //Console.WriteLine(vLong);
+
+            int vInt = 10;
+            short vShort = (short)vInt;
+
+            Console.WriteLine(vShort);
+        }
+    }
+}
