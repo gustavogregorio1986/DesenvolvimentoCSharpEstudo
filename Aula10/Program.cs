be501a031399e8f220fb45bfe52bf87b6906d606
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-01-28 13:16:12 -0300

    Desenvolvimento de enum na logica de programação

---
+++ Aula10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aula10
+{
+    public class Program
+    {
+        enum DiasSemanas { Domingo, Segunda, Terca, Quarta, Quinta, Sexta, Sabado };
+
+        static void Main(string[] args)
+        {
+           DiasSemanas ds = DiasSemanas.Domingo;
+
+           Console.WriteLine(ds);
+
+            DiasSemanas ds1 = (DiasSemanas)3;
+
+            Console.WriteLine(ds1);
+
+            DiasSemanas ds2 = (DiasSemanas)0;
+
+            Console.WriteLine(ds2);
+
+        }
+    
+    }
+}
