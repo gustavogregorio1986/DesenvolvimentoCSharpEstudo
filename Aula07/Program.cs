13a3a8a5ed155c7aa740815379fbf873ee2cccbc
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-01-28 10:05:39 -0300

    Desenvolvimento de logica para estudp!

---
+++ Aula07/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string canal = "CFB Cursos";
+            const double pi = 3.1415;
+
+
+            Console.WriteLine("Canal {0} ", canal);
+        }
+    }
+}
