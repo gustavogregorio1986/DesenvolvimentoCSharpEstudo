c9dcee17a02b83986baab5f4da4dc047ab3cc980
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-01-28 11:00:39 -0300

    Desenvolvimento de bitose no projeto de logica

---
+++ Aula08/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int v1, v2, soma;
+            string nome;
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            v1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor: ");
+            v2 = Convert.ToInt32(Console.ReadLine());
+            soma = v1 + v2;
+            Console.WriteLine("A soma de {0} mais {1} é igual a {2}", v1,v2, soma);
+        }
+    }
+}
---
+++ Aula09/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Aula09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+
+            num = num << 1;
+
+            Console.WriteLine(num);
+
+            int num1 = 10;
+
+            num1 = num1 >> 1;
+
+            Console.WriteLine(num1);
+
+            int num2 = 20;
+
+            num2 = num2 >> 1;
+
+            Console.WriteLine(num2);
+
+            int num3 = 20;
+
+            num3 = num3 << 2;
+
+            Console.WriteLine(num3);
+        }
+    }
+}
