9c8b42c17e0211bdfa5b41537f9bd5ceb0255f19
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-04 15:17:01 -0300

    Desenvolvendo for com vetor e matriz

---
+++ Aula19/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Aula19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for(e1; e2; e3)
+            //{
+
+            //}
+
+
+            int[] num = new int[10];
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                num[i] = i;
+            }
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine("Valor de num na pos{0} : {1}",i,num[i]);
+            }
+
+            //for (int num = 0; num < 10; num++)
+            //{
+            //    Console.WriteLine("Va,lor de num: {0}", num);
+            //}
+        }
+    }
+}
