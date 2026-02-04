5f5b5e9daa8cc6b6bcf1710da62a6d3be3ae0f82
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-04 10:57:08 -0300

    Desenvolvimen to de logica de programação utilizando o goto

---
+++ Aula16/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Aula16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempo = 0;
+            char escolha = ' ';
+
+        inicio:
+
+            Console.Clear();
+
+            Console.WriteLine("Belo Horizonte a Vitória/ES");
+            Console.WriteLine("Escolha o transporte: [a]-aviao | [c] - carro | [o] - onibus ");
+
+            escolha = Char.Parse(Console.ReadLine());
+
+            switch (escolha)
+            {
+                case 'a':
+                case 'A':
+                    tempo = 50;
+                    break;
+                case 'c':
+                case 'C':
+                    tempo = 480;
+                    break;
+                case 'o':
+                case 'O':
+                    tempo = 660;
+                    break;
+                default:
+                    tempo = -1;
+                    break;
+
+            }
+
+            if (tempo < 0)
+            {
+                Console.WriteLine("Transporte indisponível");
+            }
+            else
+            {
+                Console.WriteLine($"O tempo de viagem é de {tempo} minutos");
+
+            }
+
+            Console.WriteLine("\nCalcular outro transporte?[s/n]: ");
+            escolha = char.Parse(Console.ReadLine());
+            if (escolha == 's' || escolha == 'S')
+            {
+                goto inicio;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Encerrando o programa...");
+            }
+        }
+    }
+}
