97c9eeb05ffa3a56203d20a4dea2639eb6e0e698
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-04 13:14:03 -0300

    Desenvolviimento de logica de programação com vetores

---
+++ Aula17/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int n1, n2, n3, n4, n5;
+            int[] n = new int[5]; //não é 1 , vai ser sempre 0 a 4
+            int[] num = new int[3] { 55,77,99 };
+            int[] numero = { 5, 10, 15, 20, 25, 66,88 };
+            string[] veiculos = new string[3];
+
+            veiculos[0] = "Carro";
+            veiculos[1] = "Avião";
+            veiculos[2] = "Navio";
+
+            n[0]=111;
+            n[1] = 11;
+            n[2] = 22;
+            n[3] = 33;
+            n[4] = 44;
+
+            Console.WriteLine(n[0]);
+
+            Console.WriteLine(num[2]);
+
+        }
+    }
+}
