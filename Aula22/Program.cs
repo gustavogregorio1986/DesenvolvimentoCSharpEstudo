ea2b7fc48c12fb5fddf5375d0edde0b2c190053c
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-05 15:29:24 -0300

    Desenvolvimento de logica de programação com  C# paara enetdner e fixar os recrusos

---
+++ Aula22/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Aula22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[5] { 11, 22, 33,44,55 };
+
+            Console.WriteLine("Imprimir com o for");
+
+            for(int i=0; i <= num.Length-1; i++)
+            {
+                Console.WriteLine(num[i]);
+            }
+
+            for (int i = 0; i <= num.Length - 1; i++)
+            {
+                num[i] = 0;
+            }
+
+            Console.WriteLine("Imprimir com o foreach");
+
+            foreach (int n in num)
+            {
+                Console.WriteLine(n);
+            }
+
+
+        }
+    }
+}
