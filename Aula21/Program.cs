1e6a2c62dec55b095c68085d81939716f8785d25
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-05 15:07:33 -0300

    Desenvolvimento de sistema para logica de programação do

---
+++ Aula21/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num = 5;
+
+            //do {
+            //    Console.WriteLine("CFB Curso");
+            //} while (num < 5);
+
+            string senha = "123";
+            string senhauser;
+            int tentativas = 0;
+
+
+            do {
+                Console.Clear();
+                Console.WriteLine("Digite a senha: ");
+                senhauser = Console.ReadLine();
+                tentativas++;
+            } while (senha != senhauser);
+
+            Console.Clear();
+            Console.WriteLine("Senha Correta, tentativas: {0}", tentativas);
+        }
+    }
+}
