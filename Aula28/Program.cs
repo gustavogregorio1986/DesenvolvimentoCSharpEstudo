eb3945bd7543ab856041c768ec5fc4ab7f4927b2
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-10 12:24:04 -0300

    Desenvolvimenfto da logica de programação com os  modificadores

---
+++ Aula28/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Aula28
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            soma(10, 2, 11, 34, 12, 3, 4, 5);
+        }
+
+        static void soma(params int[] n)
+        {
+            int res = 0;
+
+            if (n.Length < 1)
+            {
+                Console.WriteLine("Não existem valores a serem soamdos");
+            }
+            else if (n.Length < 2)
+            {
+                Console.WriteLine("Valores insuficientes para soma.");
+            }
+            else
+            {
+                for (int i = 0; i < n.Length; i++)
+                {
+                    res += n[i];
+                }
+
+                Console.WriteLine("A soma dos valores é: {0}", res);
+            }
+        }
+    }
+}
