421bbe8643ffb9643773bb49410df37dcab271bd
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-11 14:03:59 -0300

    Desenvolvimento da lógica de programação utilizando o this

--- Aula32/Program.cs
+++ Aula32/Program.cs
@@ -2,11 +2,32 @@
 
 namespace Aula32
 {
-    internal class Program
+    class Calculos
+    {
+        public int v1;
+
+        public int v2;
+
+        public Calculos(int v1, int v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public int Somar()
+        {
+            return v1 + v2;
+        }
+    }
+
+
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calculos calculos = new Calculos(10, 2);
+
+            Console.WriteLine(calculos.Somar());
         }
     }
 }
