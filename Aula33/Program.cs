7bcd6d447d8fbe1ab56c0522a5d0be51fd60b8c4
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-11 20:00:40 -0300

    Desenvolvimento paar mostrar o public e private

---
+++ Aula33/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aula33
+{
+    class Jogador
+    {
+        public int energia;
+
+        public string nome;
+
+        public Jogador(string nome)
+        {
+            this.nome = nome;
+            energia = 100;
+        }
+    }
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Jogador j1 = new Jogador("Bruno");
+
+            Console.WriteLine("Nome.....: {0}", j1.nome);
+            Console.WriteLine("Energia..: {0} ", j1.energia);
+
+        }
+    }
+}
