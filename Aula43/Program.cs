1a14effba6413dd616a5b413eaa038d1a3e27e1d
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-13 17:15:05 -0300

    Desenvolvimento de logica de programação de interface

---
+++ Aula43/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Aula43
+{
+
+    public interface Veiculo {
+        void ligar();
+
+        void desligar();
+
+        void info();
+    }
+
+    public interface Combate
+    {
+        void disparar();
+    }
+
+    class Carro : Veiculo, Combate
+    {
+        public bool ligado;
+
+        public int municao;
+
+        public Carro()
+        {
+            setMunbicao(100);
+        }
+
+        public void setMunbicao(int qtde)
+        {
+            this.municao = qtde;
+        }
+
+        public void desligar()
+        {
+            this.ligado = false;
+        }
+
+        public void disparar()
+        {
+            
+        }
+
+        public void info()
+        {
+            
+        }
+
+        public void ligar()
+        {
+            this.ligado = true;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           Carro carro = new Carro();
+            Console.WriteLine(carro);
+        }
+    }
+}
