4223dc81fce9151cc2eaa45990f6826e78b10b25
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-13 16:06:05 -0300

    Desenvolvimento de logica de programação com indice

---
+++ Aula42/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Aula41
+{
+    class Carro
+    {
+        private int[] velMax = new int[5] { 80, 120, 160, 240, 300 };
+
+        public Carro()
+        {
+            
+        }
+
+        public int this[int i]
+        {
+            get
+            {
+                return velMax[i];
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    this.velMax[i] = value;
+                }
+                else if (value > 300)
+                {
+                    velMax[i] = 300;
+                }
+                else
+                {
+                    velMax[i] = value;
+                }
+            }
+        }
+    }
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro();
+            carro[4] = 200;
+
+            Console.WriteLine("Velocidade:{0}",carro[4]);
+        }
+    }
+}
