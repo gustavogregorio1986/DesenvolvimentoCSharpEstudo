23ea8171294201127adc53bf7ccb61e0057f1752
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-12 22:37:11 -0300

    Desenvolvimento de logica de programação com public e preotected

---
+++ Aula36/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Aula36
+{
+    class Veiculo { //Base
+        public int VelAtual;
+
+        private int velMax;
+
+        protected bool ligado;
+
+        public Veiculo(int velMax)
+        {
+            VelAtual = 0;
+            this.velMax = velMax;
+            ligado = false;
+        }
+
+        public bool getLigado()
+        {
+            return ligado;
+        }
+
+        public int getVelMax()
+        {
+            return velMax;
+        }
+    }
+
+
+    class Carro : Veiculo { //Derivada
+        public string nome;
+        public Carro(string nome, int vm) : base(vm)
+        {
+            this.nome = nome;
+            ligado = true;
+        }
+    }
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro("Bonitão", 120);
+
+            Console.WriteLine("Nome.........:{0}",carro.nome);
+            Console.WriteLine("VelMaxima....:{0}", carro.getVelMax());
+            Console.WriteLine("Vel.Máxima{0}:{0}", carro.getLigado());
+        }
+    }
+}
