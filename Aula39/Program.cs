ff9bfea279ed0944d7d338fb6618f233532ee7be
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-13 12:22:39 -0300

    Desenvolver a logica com a classe abstrata, paar consolidar os conceitos

---
+++ Aula39/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Aula39
+{
+    abstract class Veiculo { // Classe base abstrata
+        protected int VelMaxima;
+        protected int VelAtual;
+        protected bool ligado;
+        public Veiculo()
+        {
+            ligado = false;
+            VelAtual = 0;
+        }
+
+        public void setLigado(bool ligado)
+        {
+            this.ligado = ligado; 
+        }
+
+        public int getVelAtual()
+        {
+            return VelAtual;
+        }
+
+        abstract public void aceleracao(int mult);
+    }
+
+    class Carro : Veiculo
+    {
+        public Carro()
+        {
+            VelMaxima = 120;
+        }
+
+        public override void aceleracao(int mult)
+        {
+           VelAtual += 10*mult;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro();
+            Console.WriteLine(carro);
+
+            carro.aceleracao(1);
+            carro.aceleracao(-1);
+
+            Console.WriteLine(carro.getVelAtual());
+        }
+    }
+}
