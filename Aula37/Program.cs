50cb684db8c34e1f943e697254fb61832a80e8e7
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-12 22:55:58 -0300

    Desenvolver a logica de horança no projeto

---
+++ Aula37/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Aula37
+{
+
+    class Base
+    {
+        public Base()
+        {
+            Console.WriteLine("Construtor da classe base");
+        }
+    }
+
+    class Derivada1 : Base
+    {
+        public Derivada1()
+        {
+            Console.WriteLine("Construtor da classe Derivada 1");
+        }
+    }
+
+    class Derivada2 : Derivada1
+    {
+        public Derivada2()
+        {
+            Console.WriteLine("Construtor da classe Derivada 2");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Derivada2 derivada2 = new Derivada2();
+            Console.WriteLine(derivada2);
+        }
+    }
+}
