9163fdff1b18c59d1aaeeae08a9adbaf1848a9d3
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-13 13:13:05 -0300

    Desenvolvimento da logica de programação com get e set na classe Carro

---
+++ Aula41/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Aula41
+{
+    class Carro {
+        private int velMax;
+
+        public Carro()
+        {
+            this.velMax = 120;
+        }
+
+        public int VM
+        {
+            get
+            {
+               return this.velMax;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    this.velMax = value;
+                }else if (value > 300)
+                {
+                    velMax = 300;
+                }
+                else
+                {
+                    velMax = value;
+                }
+            }
+        }
+    }
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro();
+            carro.VM = 1;
+
+            Console.WriteLine(carro.VM);
+        }
+    }
+}
