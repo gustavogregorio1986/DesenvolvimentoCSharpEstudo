53e9c746efa9e2057c1982cbb82fbe86b64da30d
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-14 18:00:59 -0300

    Desenvolvimento de logica de programação de delegates

---
+++ Aula50/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Aula50
+{
+    public class Program
+    {
+        // Delegate que recebe dois inteiros e retorna um inteiro
+        delegate int Op(int n1, int n2);
+
+        class Mat
+        {
+            public static int soma(int n1, int n2)
+            {
+                return n1 + n2;
+            }
+
+            public static int mult(int n1, int n2)
+            {
+                return n1 * n2;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int res;
+
+            // Criando delegate para soma
+            Op d1 = new Op(Mat.soma);
+            res = d1(10, 5);
+            Console.WriteLine("Soma: {0}" + res);
+
+            // Criando delegate para multiplicação
+            Op d2 = new Op(Mat.mult);
+            res = d2(10, 5);
+            Console.WriteLine("Multiplicação: {0}" + res);
+        }
+    }
+}
