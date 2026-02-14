ec46260553788893018bd5fa3a41cc4c958f78c2
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-14 17:33:49 -0300

    Logica de programação com metodos e variáveis estáticos

---
+++ Aula49/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aula49
+{
+    class Mat
+    {
+        public static double PI = 3.14;
+
+        public static int dobro(int n)
+        {
+            return n * 2;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double vpi = Mat.PI;
+            int num = 10;
+
+            //Mat m1 = new Mat(); Não precisa fazer a instancia do objeto pois o metodo dobro esta  como static 
+
+            Console.WriteLine(vpi);
+            Console.WriteLine(Mat.dobro(num));
+        }
+    }
+}
