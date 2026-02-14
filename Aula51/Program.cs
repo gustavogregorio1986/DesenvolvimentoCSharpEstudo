938fa9f010a4c54cc722aa4ae36415b49fdcce4a
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-14 20:36:01 -0300

    Desenvolvimento de logica de programação

---
+++ Aula51/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Aula51
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Quangtidade de argumentos {0}", args);
+            }
+            else
+            {
+                Console.WriteLine("Não foram passados os argumentos");
+            }
+        }
+    }
+}
