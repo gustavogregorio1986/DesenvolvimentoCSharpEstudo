25be01ffc0a3c605bff2e1c5e28f90bea4d2f7f7
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-14 21:13:04 -0300

    Logica de programação com try/cacth para desenvolvimento de sistemas de software

---
+++ Aula52/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aula52
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, res;
+
+            res = n1 = n2 = 0;
+            n1 = 10;
+            n2 = 0;
+            try
+            {
+                res = n1 / n2;
+
+                Console.WriteLine("{0}/{1}={2}", n1, n2, res);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ocorreu erro: {0}", e.Message);
+                Console.WriteLine("Ex: {0}", e.GetType());
+            }
+            
+        }
+    }
+}
