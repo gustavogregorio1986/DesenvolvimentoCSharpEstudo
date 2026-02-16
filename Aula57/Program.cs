8d628addbba561c4997c1157d528270ed02d23ca
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-16 09:25:29 -0300

    FDesenvolvimento de litsa 1 e dois com exemplo na logica

---
+++ Aula57/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula57
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> carros = new List<string>();
+            List<string> carros2 = new List<string>();
+            string[] carros3 = new string[4];
+
+            carros.Add("Golf");
+             carros.Add("HRV");
+             carros.Add("Focus");
+             carros.Add("Argo");
+
+            carros2.AddRange(carros);
+
+            //carros.Clear();
+            //carros.Clear();
+
+            if (carros.Contains("Gol"))
+            {
+                Console.WriteLine("Esta na lista");
+            }
+            else
+            {
+                Console.WriteLine("Não encontrado");
+            }
+
+            carros.CopyTo(carros3, 2);
+
+            foreach (string c in carros)
+            {
+                Console.WriteLine("Carro: {0}",c);
+            }
+
+            Console.WriteLine("Mesma lista");
+
+            foreach (string c in carros2)
+            {
+                Console.WriteLine("Carro: {0}", c);
+            }
+        }
+    }
+}
