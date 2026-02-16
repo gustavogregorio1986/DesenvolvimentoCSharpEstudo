9adb691eafead54769352fb387fed69f634e101c
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-16 13:48:36 -0300

    Desenvolvendo fila com Quere na logica de programação

---
+++ Aula59/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Aula59
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] vs = {"Carro","Moto","Navio","Aviao"};
+            //Queue<string> veiculos = new Queue<string>(v);
+            Queue<string> veiculos = new Queue<string>();
+
+            veiculos.Enqueue("Carro");
+            veiculos.Enqueue("Moto");
+            veiculos.Enqueue("Navio");
+            veiculos.Enqueue("Aviao");
+
+            //string v = "Patinete";
+            //if (veiculos.Contains(v))
+            //{
+            //    Console.WriteLine("Veiculo " + v + " encontrado");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Veiculo " + v + " não esta na fila  encontrado");
+            //}
+
+            //veiculos.Clear();
+            //Console.WriteLine("Primeiro Veiculo " + veiculos.Dequeue());
+            //Console.WriteLine("Primeiro Veiculo " + veiculos.Peek());
+            //Console.WriteLine("Tamanho Fila: " + veiculos.Count);
+
+            foreach (string v in veiculos)
+            {
+                veiculos.Dequeue();
+                Console.WriteLine("Veiculo removido: " + v);
+            }
+        }
+    }
+}
